71d646931360a728eaeb10e390a412979015fdcd
Mark O'Reilly <marko@megsupporttools.com>
2021-11-03 21:05:59 +0100

    added constants

---
+++ constants.py
@@ -0,0 +1,19 @@
+API_URL = "https://api.binance.com"
+BASE_CURRENCY_BUSD = "BUSD"
+PRICE_DATA_COLUMNS = {
+    'time_open': "O",
+    'open': "f",
+    'high': "f",
+    'low': "f",
+    'Close': "f",
+    'volume': "f",
+    'time_close': "O",
+    'quote_asset_volume': "f",
+    'number_of_trades': "f",
+    'taker_buy_base_asset_volume': "f",
+    'taker_buy_quote_asset_volume': "f",
+    'to_be_ignored': "f"
+}
+STOCK_INFO_52_WEEK_HIGH = "fiftyTwoWeekHigh"
+STOCK_INFO_52_WEEK_LOW = "fiftyTwoWeekLow"
+STOCK_INFO_PREV_CLOSE = "previousClose"
