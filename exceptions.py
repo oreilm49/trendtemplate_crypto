e7308a750935ddf68e78527a95e3d0d2a194b41c
Mark O'Reilly <marko@megsupporttools.com>
2021-11-10 21:00:40 +0100

    moved exceptions into a file

---
+++ exceptions.py
@@ -0,0 +1,14 @@
+class DowntrendingException(Exception):
+    pass
+
+
+class InvalidTicker(Exception):
+    pass
+
+
+class InvalidBaseCurrency(Exception):
+    pass
+
+
+class DuplicatedArgs(Exception):
+    pass
